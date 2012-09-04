02f92168f9707b5d1b3e47d5be3ee4e92a1852cd
davros <davros@droidconcepts.com>
2012-09-04 09:49:49 -0400

    fixing maguro build

--- products/maguro.mk
+++ products/maguro.mk
@@ -11,23 +11,15 @@ PRODUCT_DEVICE := maguro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=yakju BUILD_ID=JRO03C BUILD_FINGERPRINT=google/takju/maguro:4.1.1/JRO03C/398337:user/release-keys PRIVATE_BUILD_DESC="takju-user 4.1.1 JRO03C 398337 release-keys" BUILD_NUMBER=235179
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=yakju BUILD_ID=JRO03L BUILD_FINGERPRINT=google/takju/maguro:4.1.1/JRO03L/398337:user/release-keys PRIVATE_BUILD_DESC="takju-user 4.1.1 JRO03C 398337 release-keys" BUILD_NUMBER=235179
 
 # Inherit common build.prop overrides
 -include vendor/Droid_Concepts/products/common_versions.mk
 
 # Copy maguro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/Droid_Concepts/proprietary/maguro/lib/libgcomm_jni.so:system/lib/libgcomm_jni.so \
-    vendor/Droid_Concepts/proprietary/maguro/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/Droid_Concepts/proprietary/maguro/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/Droid_Concepts/proprietary/maguro/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/etc/smc_normal_world_android_cfg.ini:system/vendor/etc/smc_normal_world_android_cfg.ini \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/firmware/smc_pa_wvdrm.ift:system/vendor/firmware/smc_pa_wvdrm.ift \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/Droid_Concepts/proprietary/maguro/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/Droid_Concepts/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/Droid_Concepts/proprietary/maguro/vendor/etc/smc_normal_world_android_cfg.ini:system/vendor/etc/smc_normal_world_android_cfg.ini 
 
 # Inherit media effect blobs
 -include vendor/Droid_Concepts/products/common_media_effects.mk
@@ -40,4 +32,3 @@ PRODUCT_COPY_FILES +=  \
 
 # Inherit speech recognition blobs
 -include vendor/Droid_Concepts/products/common_speech_recognition.mk
-
