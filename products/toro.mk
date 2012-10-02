2d18f325d2f47a118a582ed58b7b68313173e341
davros <davros@droidconcepts.com>
2012-10-02 07:52:46 -0400

    ringtones vendor add

--- products/toro.mk
+++ products/toro.mk
@@ -21,6 +21,8 @@ PRODUCT_COPY_FILES +=  \
     vendor/Droid_Concepts/proprietary/tuna/app/Thinkfree.apk:system/app/Thinkfree.apk \
     vendor/Droid_Concepts/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
     vendor/Droid_Concepts/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/Droid_Concepts/proprietary/tuna/media/audio/notifications/Simple_High.mp3:system/media/audio/notifications/Simple_High.mp3 \
+    vendor/Droid_Concepts/proprietary/tuna/media/audio/ringtones/Provincial_Synthetic.ogg:system/media/audio/ringtones/Provincial_Synthetic.ogg \
     vendor/Droid_Concepts/proprietary/common/app/Wallet.apk:system/app/Wallet.apk
 
 # Copy vzw login 
