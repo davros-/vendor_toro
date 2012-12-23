23b706c244813ee1840b9fe10487a17c4590188a
davros <davros@droidconcepts.com>
2012-12-23 09:27:03 -0500

    edit version names

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -3,10 +3,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=relea
 
 
 # Additional properties
-PRODUCT_VERSION_MAJOR = 10
+PRODUCT_VERSION_MAJOR = 11
 PRODUCT_VERSION_MINOR = 0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.modversion=Droid_Concepts-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-NIGHTLY-$(shell date +"%m-%d-%y")
+ro.modversion=Droid_Concepts-$(shell date +"%m-%d-%y")
     
 
