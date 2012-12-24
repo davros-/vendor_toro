c8c654443a71dbc5f0b564403668623db5ccddc7
davros <davros@droidconcepts.com>
2012-12-24 03:41:33 -0500

    build id 2

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=release-keys USER=davros BUILD_UTC_DATE=$(shell date +"%s")
 
 
 # Additional properties
@@ -10,5 +10,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.modversion=Droid_Concepts-$(shell date +"%m-%d-%y")
     
 #Uncomment the below lines and insert name to be displayed as Developer Id
-PRODUCT_BUILD_PROP_OVERRIDES += \
-ro.build.host=davros
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#ro.build.host=davros
