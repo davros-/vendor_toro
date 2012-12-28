3ced706baf257c1660dcc805732e444d7129236d
davros <davros@droidconcepts.com>
2012-12-28 14:58:36 -0500

    DCBeats cphelps76

--- products/beats_audio.mk
+++ products/beats_audio.mk
@@ -1,7 +1,7 @@
 # Beats Audio
 PRODUCT_COPY_FILES += \
     vendor/Droid_Concepts/proprietary/common/beats/app/AudioEffectService.apk:system/app/AudioEffectService.apk \
-    vendor/Droid_Concepts/proprietary/common/beats/app/DEMENTEDBeats.apk:system/app/DEMENTEDBeats.apk \
+    vendor/Droid_Concepts/proprietary/common/beats/app/DCBeats.apk:system/app/DCBeats.apk \
     vendor/Droid_Concepts/proprietary/common/beats/bin/basimage_gec.bin:system/bin/basimage_gec.bin \
     vendor/Droid_Concepts/proprietary/common/beats/bin/basimage_gec_x.bin:system/bin/basimage_gec_x.bin \
     vendor/Droid_Concepts/proprietary/common/beats/bin/basimage_ibeats.bin:system/bin/basimage_ibeats.bin \
