5a8307e54e7f11a143e19af3fbaba2af187678e6
davros <davros@droidconcepts.com>
2013-01-30 13:38:12 -0500

    Revert "DC Beats sourcey source"
    This reverts commit 9898c95296f67f2ba0bb83769017cb1a8957b38a.

--- products/common.mk
+++ products/common.mk
@@ -20,8 +20,7 @@ include vendor/Droid_Concepts/products/themes_common.mk
 # Common packages 
 PRODUCT_PACKAGES += \
     DCWallpapers \
-    Chronus \
-    DCBeats
+    Chronus
 
 # Common Google Applications and libs
 PRODUCT_COPY_FILES += \
