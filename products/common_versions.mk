e50b218e241f25d76a332707d36cb3f4dc841afb
davros <davros@droidconcepts.com>
2013-02-06 03:58:54 -0500

    build ID

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=release-keys USER=davros BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40G BUILD_VERSION_TAGS=release-keys USER=davros BUILD_UTC_DATE=$(shell date +"%s")
 
 
 # Additional properties
