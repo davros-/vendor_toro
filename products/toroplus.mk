120cbc6c38856e7af3cac33de8f2abacc222854c
davros <davros@droidconcepts.com>
2013-02-26 11:41:53 -0500

    toroplus stuff

--- products/toroplus.mk
+++ products/toroplus.mk
@@ -1,34 +1,42 @@
-# Inherit AOSP device configuration for toroplus.
+# device configuration for toroplus.
 $(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
-
-# Inherit common product files.
 $(call inherit-product, vendor/Droid_Concepts/products/common.mk)
+$(call inherit-product, vendor/Droid_Concepts/products/themes_common.mk)
+
+# Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/Droid_Concepts/overlay/toroplus
+PRODUCT_PACKAGE_OVERLAYS += vendor/Droid_Concepts/overlay/common
 
 # Setup device specific product configuration.
 PRODUCT_NAME := Droid_Concepts_toroplus
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_DEVICE := toroplus
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT=samsung/mysidspr/toroplus:4.0.4/IMM76I/330937:user/release-keys PRIVATE_BUILD_DESC="mysidspr-user 4.0.4 IMM76I 330937 release-keys" BUILD_NUMBER=330937
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40D/L700GA02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.2.1 JOP40D L700GA02 release-keys"
 
-# Inherit common build.prop overrides
--include vendor/Droid_Concepts/products/common_versions.mk
-
-# Copy maguro specific prebuilt files
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-verizon \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.mode=OPTIONAL \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+    ro.gsm.data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
+    ro.gsm.2nd_data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
+    ro.cdma.otaspnumschema=SELC,1,80,99 \
+    ro.com.android.dataroaming=false \
+    drm.service.enabled=true \
+    camera.flash_off=0 \
+    dalvik.vm.lockprof.threshold=500 \
+    wifi.supplicant_scan_interval=180 
+    
+# Copy toroplus specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/Droid_Concepts/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/Droid_Concepts/proprietary/tuna/app/Gallery2.apk:system/app/Gallery2.apk \
-  
-# Inherit media effect blobs
--include vendor/Droid_Concepts/products/common_media_effects.mk
+    vendor/Droid_Concepts/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip 
 
-# Inherit Face lock security blobs
--include vendor/Droid_Concepts/products/common_facelock.mk
+# Torospr specific packages
+PRODUCT_PACKAGES += \
+    Thinkfree
 
-# Inherit drm blobs
--include vendor/Droid_Concepts/products/common_drm_phone.mk
 
-# Inherit speech recognition blobs
--include vendor/Droid_Concepts/products/common_speech_recognition.mk
